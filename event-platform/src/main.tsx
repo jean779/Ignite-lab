9fb6fb15dbcc086fd6fa58c00069fbcc77f58e04
jean.fernandes <jean.fernandes@esig.com.br>
2022-06-24 13:33:59 -0300

    finalização aula03

--- event-platform/src/main.tsx
+++ event-platform/src/main.tsx
@@ -1,14 +1,18 @@
 import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { cliente } from './lib/apollo'
+import { Router } from './Router'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ApolloProvider client={cliente}>
-      <App />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>
 )
